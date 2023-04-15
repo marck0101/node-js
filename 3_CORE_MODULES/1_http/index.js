9cbc8b1008197ae758d77389fcb0fa4e0449705c
Marcos <marcos.henrique.a7x@@gmail.com>
2023-04-15 09:38:02 -0300

    básico para uma requisicao web

--- 3_CORE_MODULES/1_http/index.js
+++ 3_CORE_MODULES/1_http/index.js
@@ -1,12 +1,13 @@
-const http = require('http')
+const http = require("http"); // importo o http
 
-const port = 3000
+const port = 3000; // crio uma porta
 
+// vai escrever uma resposta para o usuário e finalizo ela
 const server = http.createServer((req, res) => {
-  res.write('Oi HTTP')
-  res.end()
-})
-
+  res.write("Oi HTTP");
+  res.end();
+});
+// para conseguir executar tudo, preciso 'escutar a porta
 server.listen(port, () => {
-  console.log(`Servidor rodando na porta: ${port}`)
-})
+  console.log(`Servidor rodando na porta: ${port}`);
+});
