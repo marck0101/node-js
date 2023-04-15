66956ea00d16aa9db3b7c57634d478ad3f862d9f
Marcos <marcos.henrique.a7x@@gmail.com>
2023-04-15 10:24:47 -0300

    'url'

--- 3_CORE_MODULES/3_url/index.js
+++ 3_CORE_MODULES/3_url/index.js
@@ -1,9 +1,10 @@
-const url = require('url')
-const address = 'https://www.meusite.com.br/catalogo?produtos=cadeira'
-const parsedUrl = new url.URL(address)
+const url = require("url");
+const address = "https://www.meusite.com.br/catalogo?produtos=cadeira";
+const parsedUrl = new url.URL(address);
 
-console.log(parsedUrl.host)
-console.log(parsedUrl.pathname)
-console.log(parsedUrl.search)
-console.log(parsedUrl.searchParams)
-console.log(parsedUrl.searchParams.get('produtos'))
+console.log(parsedUrl.host);
+console.log(parsedUrl.pathname);
+console.log(parsedUrl.search);
+console.log(parsedUrl.searchParams);
+// chama produto(cadeira) atraves da chave url(produtos)
+console.log(parsedUrl.searchParams.get("produtos"));
