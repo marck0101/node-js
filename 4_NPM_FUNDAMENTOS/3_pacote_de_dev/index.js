ffb3e567ccbca5ee9cf464334606ef2228b028af
marck0101 <marck.mhc@gmail.com>
2023-05-03 20:25:49 -0300

    tesste

--- 4_NPM_FUNDAMENTOS/3_pacote_de_dev/index.js
+++ 4_NPM_FUNDAMENTOS/3_pacote_de_dev/index.js
@@ -3,7 +3,7 @@ const chalk = require("chalk");
 
 const a = [1, 2, 3];
 const b = [4, 5, 6];
-
+// método para verificar a diferença entre dois arrays
 const diff = _.merge(a, b);
 
 console.log(chalk.red.bold(diff));
